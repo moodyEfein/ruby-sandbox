66c471c2487d91988ce571593b0058b1e4ce7937
Moody Efein <98859197+moodyEfein@users.noreply.github.com>
2023-11-30 17:01:47 +0000

    initial commit

---
+++ howdy.rb
@@ -0,0 +1,13 @@
+my_string = "Howdy, there!"
+
+pp my_string
+
+require "./goodbye.rb"
+require "active_support/all"
+
+pp 1.ordinalize
+pp 2.ordinalize
+
+pp "table".pluralize
+pp "rubies".singularize
+
